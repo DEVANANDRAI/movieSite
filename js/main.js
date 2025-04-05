7027b761a0894332c293cbe7392b68cc0fa6c825
Devanand Rai <devanandrai3260@gmail.com>
2025-04-05 20:43:20 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -22,7 +22,7 @@ $('svg').click(function() {
 
 
  
-let api="https://imdb.iamidiotareyoutoo.com/search?q="
+let api="https://imdb.iamidiotareyoutoo.com/search?q=thriller"
 const getData=(api)=>{
   const response= fetch(api)
 response.then((value)=>{
